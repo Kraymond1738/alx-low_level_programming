c3b872334296a8accc08f406a85d26cfeaa06128
Kasumba <kraymond0756519001@gmail.com>
2022-09-09 18:47:05 +0000

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lower case
+ *
+ * Return: always a 0
+ */
+int main(void)
+
+{
+	char ab;
+
+	for (ab = 'a' ; ab <= 'z' ; ab++)
+	{
+		putchar(ab);
+	}
+putchar('\n');
+return (0);
+}
