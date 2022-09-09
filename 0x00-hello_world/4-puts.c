591082fca80de54483b213f9783b39d7e51d9d7a
Kasumba Raymond <kasumbaraymond0756519001@gmail.com>
2022-09-09 11:54:14 +0300

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  * Return: 0 (successful)
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle\n");
+{puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
