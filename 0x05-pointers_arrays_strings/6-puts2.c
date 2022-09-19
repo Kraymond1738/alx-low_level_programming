1856e9cb7c88016d5486674e67809c3d397bdc8e
Kasumba <kraymond1738@gmail.com>
2022-09-19 19:36:18 +0000

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,16 +7,16 @@
 void puts2(char *str)
 {
 	int a = 0;
-	int b;
+	int b = 1;
 
 	while (str[a] != '\0')
 	{
-		b = str{a};
 		if (b % 2 = 0)
 		{
 			_putchar(str[a]);
 		}
+		b++;
 		a++;
 	}
-	putchar('\n');
+	_putchar('\n');
 }
