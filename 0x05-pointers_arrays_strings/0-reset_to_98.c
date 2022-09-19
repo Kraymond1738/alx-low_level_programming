892d8a03e362a775701a8fc5dae5f9fdd916304a
Kasumba <kraymond1738@gmail.com>
2022-09-19 10:45:47 +0000

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,12 +9,10 @@ int main(void)
 	int n = 402;
 	int  *pt = &n;
 
-	_putchar("n=%d\n", n + '0');
+	_putchar("n=%d\n", *pt);
 
 	reset_to_98(&n);
 
-	_putchar("n=%d\n", *pt + '0');
+	_putchar("n=%d\n", *pt);
 	return (0);
 }
-
-
