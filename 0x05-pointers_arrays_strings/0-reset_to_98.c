a25ab90285ac146ad3db2bbb5d6593d7b7fc54df
Kasumba <kraymond1738@gmail.com>
2022-09-19 11:06:15 +0000

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,6 +2,10 @@
 /**
  * main - pointers
  *
+ * @n: this is a value which we are going to point to using *n
+ *
+ * Description: This code checks for the value it location &n and assigns the it a new value 98
+ *
  * Return: zero
  */
 void reset_to_98(int *n)
