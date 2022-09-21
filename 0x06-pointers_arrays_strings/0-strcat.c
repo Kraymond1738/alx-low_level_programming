df3ef72231edf1a56891b62db5589a3b4c5c6aa8
Kasumba <kraymond1738@gmail.com>
2022-09-21 19:55:28 +0000

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ *@dest: where the string is going to be written
+ *@src: string that are going to be joine
+ * Return: Returns a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	int i = 0;
+	int a;
+
+	while (src[i] != '\0')
+	{
+		++len;
+		i++;
+	}
+
+	a = 0;
+	while (src[a] != '\0')
+	{
+		dest[len + a] = src[a];
+		a++;
+	}
+	dest[len + a] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains codes to 0x06-pointers_arrays_strings
