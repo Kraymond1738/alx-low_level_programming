2c55502ef7e5c5a8a0f91e28190de71ceb452150
Kasumba <kraymond1738@gmail.com>
2022-09-21 21:19:35 +0000

    Task 2, 3, 4

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,23 @@
-#include "main.h"
-/**
- * _strcmp - copies two strings
- * @s1: string 1
- * @s2: string 2
- * Return: zero
- */
+#include <stdio.h>
 
-int _strcmp(char *s1, char *s2)
+int main(void)
 {
-	int i = 0;
-	int a = 0;
+  int n;
+  int a[5];
+  int *p;
 
-	while (a == 0)
-	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-			break;
-		a = *(s1 + i) - *(s2 + i);
-		i++;
-	}
-
-	return (a);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
