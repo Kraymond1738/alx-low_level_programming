a5b90fe8b7d22b25024e3717b5544f0a5bbfae8f
Kasumba Raymond <kasumbaraymond0756519001@gmail.com>
2022-09-26 12:53:07 +0300

    Readme file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+#include <main.h>
+/**
+ * _memset - that fills memory with a constant byte
+ * @s: pointer
+ * @b: 
+ * @n:
+ * Return: nothing
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Contains answers to 0x07-pointers_arrays_strings AlX SE
